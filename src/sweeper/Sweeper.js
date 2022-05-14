c89f1b17cb905cbb15dd0018ebe6270996a45d6f
Frank Chen <cheng.frank.chen@gmail.com>
2022-05-14 12:02:39 -0400

    some changes

---
+++ src/sweeper/Sweeper.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SweeperBoard from './SweeperBoard'
+
+export default class Sweeper extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        console.log("wut")
+        return (
+            <SweeperBoard />
+        )
+    }
+}
---
+++ src/sweeper/SweeperBoard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class SweeperBoard extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <p>I am a board.</p>
+        )
+    }
+}
